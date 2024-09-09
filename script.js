ed311cee21a23dc88480da12b1e4cde958e98a02
brian <yangbri1@msu.edu>
2024-09-09 17:38:15 -0400

    calculations

--- script.js
+++ script.js
@@ -9,12 +9,34 @@
 // given information 
 let radius = 5; // units: meters 
 const PI = 3.1415;
-const area = PI * radius * radius; // (area of a circle) units: meters**2
-let plant_space = 0.8 // m**2
+const total_area = PI * radius * radius; // (area of a circle) units: meters**2
+let plant_area = 0.8 // m**2
 let sample_size = 20;
-let weeks = 2;
+let weeks;
 
 // calculates number of plants after nth weeks
-let growth_rate = sample_size * 2**weeks;
+let plant_count = sample_size * 2**weeks;
 
-console.log(`At week ${weeks}, there are ${growth_rate} plants.`);
+// calculates maximum capacity of the alloted space
+let max_capacity = total_area / plant_area;
+console.log(`Max amount of plants`, max_capacity);
+// calculates occupied areas by plants
+let space_taken = plant_count * plant_area;
+
+// console.log() out total available space for the plants
+console.log(`Area of sample size`, total_area);
+
+eighty_capacity = max_capacity * 0.8;
+fifty_capacity = max_capacity * 0.5;
+
+console.log(eighty_capacity, fifty_capacity);
+
+// Week 1 Results:
+weeks = 1;
+plant_count = sample_size * 2**weeks;
+space_taken = plant_count * plant_area;
+console.log(`Week 1 Results:`);
+console.log(`${plant_count} plants.`);
+console.log(space_taken);
+
+
